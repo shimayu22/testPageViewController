8b38991797a1f4da5faa751f6ffc8e27e1c4f981
Yutaka Shimada <shimayu22@gmail.com>
2018-09-22 21:26:37 +0900

    Initial Commit

---
+++ testPageViewController/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  testPageViewController
+//
+//  Created by Yutaka Shimada on 2018/09/22.
+//  Copyright © 2018年 shimayu. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
